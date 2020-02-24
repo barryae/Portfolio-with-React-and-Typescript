9dcece7dca7b2fd1fd4a971e8baf867cd919f9bb
Green Vine <barryaervin@gmail.com>
2020-02-23 19:20:38 -0500

    added basic bio

--- app/src/pages/Main.tsx
+++ app/src/pages/Main.tsx
@@ -1,26 +1,24 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import { Projects } from "../components/Projects"
+import { Bio } from "../components/Bio"
 
 export const Main = () => {
 
     const [data, setData] = useState([])
     const gitHubParams: string = "visibility=public&sort=pushed"
-
     useEffect(() => {
         const fetchRepos = async () => {
             const list = await fetch("https://api.github.com/users/barryae/repos?" + gitHubParams);
             const repos = await list.json();
-            console.log(repos)
             setData(repos.slice(0, 3))
         }
         fetchRepos()
     }, []);
-
-
     console.log(data)
     return (
         <div >
+            <Bio />
             <Projects projects={data} />
         </div>
     )
