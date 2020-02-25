9d57253f9013c9a816d44f1fe7903d4069088985
Green Vine <barryaervin@gmail.com>
2020-02-25 15:12:20 -0500

    Updates

---
+++ precache-manifest.9b1b3e6485c51b9d7d9c05237af8be7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafe134633bceea1a807fb8b20641507",
+    "url": "/Portfolio-with-React-and-Typescript/index.html"
+  },
+  {
+    "revision": "6ca5a3be3fa535d7f16e",
+    "url": "/Portfolio-with-React-and-Typescript/static/css/main.d36156bb.chunk.css"
+  },
+  {
+    "revision": "b6a88435cd237fa36292",
+    "url": "/Portfolio-with-React-and-Typescript/static/js/2.c10dbb00.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Portfolio-with-React-and-Typescript/static/js/2.c10dbb00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca5a3be3fa535d7f16e",
+    "url": "/Portfolio-with-React-and-Typescript/static/js/main.66ef0cf0.chunk.js"
+  },
+  {
+    "revision": "df74e57b1cd50fc509de",
+    "url": "/Portfolio-with-React-and-Typescript/static/js/runtime-main.c443f0f9.js"
+  },
+  {
+    "revision": "a044935bb29db276f19e5593a53f004d",
+    "url": "/Portfolio-with-React-and-Typescript/static/media/project0.a044935b.png"
+  },
+  {
+    "revision": "fbde7378c2267816d35777c3c2d14254",
+    "url": "/Portfolio-with-React-and-Typescript/static/media/project1.fbde7378.png"
+  },
+  {
+    "revision": "b5388a7d2c2df1b4dd9d1a3428227fbe",
+    "url": "/Portfolio-with-React-and-Typescript/static/media/project2.b5388a7d.png"
+  }
+]);
