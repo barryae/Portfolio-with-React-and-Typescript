bf69dd4a3d4df0019cff687da2000c7a93ac9dd9
Green Vine <barryaervin@gmail.com>
2020-02-26 11:50:28 -0500

    text editing

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -8,7 +8,7 @@ export function Bio() {
                 <div className="image-container">
                     <img className="bio-img" src="https://avatars2.githubusercontent.com/u/55558517?v=4" alt="Avatar" />
                 </div>
-                <p className="bio-text">I am a Full Stack Web Developer creating inspired web-apps using React, JavaScript, and Node. I love quickly adapting to interesting situations involving rapidly changing technology. Let's build something great together.</p>
+                <p className="bio-text">Full Stack Web Developer creating inspired web-apps using React, JavaScript, and Node. I love quickly adapting to interesting situations involving rapidly changing technology. Let's build something great together.</p>
             </div>
         </div >
     )
