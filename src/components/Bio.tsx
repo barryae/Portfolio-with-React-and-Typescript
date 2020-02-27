d3ea06e87786b32634c7a9798cb1e80dd9ab1dcf
Green Vine <barryaervin@gmail.com>
2020-02-27 16:15:07 -0500

    copy editing

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -7,11 +7,15 @@ export default function Bio() {
             <div className="top-spacer"></div>
             <BigWord word="Hey, I'm Barry." />
             <div className="content">
-                <p className="bio-text">I'm a Full Stack Web Developer based in Atlanta, GA.</p>
                 <p className="bio-text">
-                    Interesting problems that require interesting technology make me get out of bed in the morning.
-                    I specialize in building with React and Node, but I'm constantly expanding my bag of tools.
-                    If you have some interesting problems that need solving, I'd love to help.
+                    I’m a Full Stack Web Developer based in Atlanta, GA.
+                </p>
+                <p className="bio-text">
+                    Unique problems that require interesting technology
+                    make me get out of bed in the morning. I specialize in
+                    building with React and Node, but I’m constantly expanding
+                    my bag of tools. If you have some interesting problems
+                    that need solving, I’d love to help.
                 </p>
             </div>
         </div >
