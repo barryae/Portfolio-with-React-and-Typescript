4f935f1c494549a8af8557ddb0b74868e7444571
Green Vine <barryaervin@gmail.com>
2020-03-04 09:42:01 -0500

    updated resume

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
             <div className="item links-container">
                 <a href="https://www.linkedin.com/in/barry-ervin/" target="_blank" rel="noopener noreferrer"> <img className="icon-img linkedin" src={require('../images/icons/001-linkedin.png')} alt="LinkedIn" /></a>
                 <a href="https://github.com/barryae" target="_blank" rel="noopener noreferrer"><img className="icon-img github" src={require('../images/icons/002-cat.png')} alt="GitHub" /></a>
-                <a href="https://drive.google.com/file/d/128y1f6NLKJVZtlFNlOJ4H0x5WNZmahMv/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img className="icon-img resume" src={require('../images/icons/003-cv.png')} alt="Resume" /></a>
+                <a href="https://drive.google.com/file/d/1tdnTphN4BW5d89-C0rhLHsmh-hi1AS7d/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img className="icon-img resume" src={require('../images/icons/003-cv.png')} alt="Resume" /></a>
             </div>
             <div className="item close-bracket">{'}'}</div>
         </div >
